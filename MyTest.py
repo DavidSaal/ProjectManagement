052053249345b79aa1ffdc7d3dad9e3e8c553360
DavidSaal <33327426+DavidSaal@users.noreply.github.com>
2019-03-27 15:26:43 +0200

    Final

--- MyTest.py
+++ MyTest.py
@@ -1,21 +1,19 @@
 import Yummly as api
 import DataBase as db
-from unittest.mock import MagicMock,patch,Mock
+from unittest.mock import MagicMock, patch, Mock
 import unittest
-
 
 def get_input(text):
     return input(text)
 
 class MyTest(unittest.TestCase):
-
     obj = api.Yummly()
     obj.method = MagicMock(side_effect=obj.mealTime)
 
     def mockConnectionBreakfast(self, Mock):
         Mock.return_value = db.breakfast
         return Mock()
-
+    
     def mockConnectionLunch(self, Mock):
         Mock.return_value = db.lunch
         return Mock()
@@ -23,7 +21,6 @@ class MyTest(unittest.TestCase):
     def mockConnectionDinner(self, Mock):
         Mock.return_value = db.dinner
         return Mock()
-
 
     @patch('DataBase.breakfast')
     def test_BreakfastRecipeNameNotNone(self, input):
@@ -61,27 +58,25 @@ class MyTest(unittest.TestCase):
         for i in Data:
             self.assertIsNotNone(i['ingredients'])
 
+    @patch('MyTest.get_input', return_value=1)
+    def test_NotReturnNoneMealTime(self, input):
+        self.assertIsNotNone(self.obj.mealTime(0, 1))
 
     @patch('MyTest.get_input', return_value=1)
-    def test_NotReturnNoneMealTime(self,input):
-        self.assertIsNotNone(self.obj.mealTime(0,1))
-
-    @patch('MyTest.get_input', return_value=1)
-    def test_NotReturnEmptyMealTime(self,input):
-        listOfIngredients = self.obj.mealTime(0,1)
+    def test_NotReturnEmptyMealTime(self, input):
+        listOfIngredients = self.obj.mealTime(0, 1)
         for i in range(len(listOfIngredients)):
             self.assertIsNotNone(listOfIngredients[i])
 
     def test_NotReturnMealForThisTime(self):
-        self.assertEqual(self.obj.method(5),'no Meal for this Time')
+        self.assertEqual(self.obj.method(5), 'no Meal for this Time')
         self.assertEqual(self.obj.method(25), 'no Meal for this Time')
 
     def test_NotReturnEmptyBuildMenu(self):
-        listOfMeal =self.obj.buildMenu(1)
+        listOfMeal = self.obj.buildMenu(1)
         for i in range(len(listOfMeal)):
             self.assertIsNotNone(listOfMeal[i])
 
-
     @patch('MyTest.get_input', return_value='yes')
-    def test_NotReturnNoneBuildMenu(self,input):
+    def test_NotReturnNoneBuildMenu(self, input):
         self.assertIsNotNone(self.obj.buildMenu(1))
